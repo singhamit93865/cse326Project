12211f9281d18eca2da3c268148b969768c30112
Amit singh <60794717+singhamit93865@users.noreply.github.com>
2020-04-12 15:52:55 +0530

    Add files via upload

---
+++ ujjual.js
@@ -0,0 +1,30 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Show more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Show less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunctions() {
+    var dots = document.getElementById("dotss");
+    var moreText = document.getElementById("mores");
+    var btnText = document.getElementById("myBtns");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Show more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Show less"; 
+      moreText.style.display = "inline";
+    }
+  }
